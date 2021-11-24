d73ba049fe6249b6687924aff8086d375cc5b568
Alvin <alvinonline@pm.me>
2021-11-24 10:01:45 +0000

    fixed prod bug, added error message, ready for launch

---
+++ readme.md
@@ -0,0 +1,14 @@
+# GeoJSON tool
+
+I always use GeoJSON.io to inspect my GeoJSONs and make sure the data is right.
+
+The problem is: It's really really slow, so I made my own replacement.
+
+- Drag & drop a GeoJSON or TopoJSON
+- Preview it on top of a real-world map.
+- Gives you an error message if it doesn't import
+- Much, much faster
+
+I can't believe I managed to make this so quickly. Credit goes to MapLibre and ViteJs
+
+If you need to reproject, run `-proj "EPSG:4326"` in the Mapshaper console.
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import bbox from '@turf/bbox';
 import maplibregl from 'maplibre-gl';
 import style from './style';
 
+// Some places to position the map on startup
+const places = [
+  [2.2935339108148383, 48.85815954565956], // Paris
+
+  [-6.147688520866393, 53.36793308040547], // Dublin
+
+  [151.21326695694927, -33.851985478211034], // Sydney
+  [139.78270173499422, 35.64721657992466], // Tokyo
+];
+
 const addGeojson = (map, geo) => {
   const layerID = 'layer_' + Math.random();
   const sourceID = 'source_' + Math.random();
@@ -33,8 +43,8 @@ const addGeojson = (map, geo) => {
   map.fitBounds(bbox(geo), { padding: 20, duration: 0 });
 
   let hoveredStateId = null;
-  // When the user moves their mouse over the state-fill layer, we'll update the
-  // feature state for the feature under the mouse.
+  // When the user moves their mouse over the layer, we'll update
+  // the feature state for the feature under the mouse.
   map.on('mousemove', layerID, (e) => {
     map.getCanvas().style.cursor = 'pointer';
 
@@ -76,38 +86,46 @@ const preventDefaults = (e) => {
   e.stopPropagation();
 };
 
-const start = async () => {
+const handleDrop = (map) => async (e) => {
+  let dt = e.dataTransfer;
+
+  for (const item of dt.items) {
+    // If dropped items aren't files, reject them
+    if (item.kind === 'file') {
+      var file = item.getAsFile();
+      let t = await file.text();
+      let j = JSON.parse(t);
+
+      try {
+        addGeojson(map, j);
+      } catch (error) {
+        alert(`
+        ${error.message}
+
+        Try re-projecting your JSON
+        `);
+      }
+    }
+  }
+};
+
+const start = () => {
+  const randomPlace = places[Math.floor(Math.random() * places.length)];
+
   const map = new maplibregl.Map({
     container: 'app',
     style,
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+    center: randomPlace, // starting position [lng, lat]
+    zoom: 11, // starting zoom
   });
 
   const nav = new maplibregl.NavigationControl();
   map.addControl(nav, 'top-left');
-
   ['dragenter', 'dragover', 'dragleave', 'drop'].forEach((eventName) => {
     document.body.addEventListener(eventName, preventDefaults, false);
   });
 
-  document.body.addEventListener('drop', handleDrop, false);
-
-  const handleDrop = async (e) => {
-    let dt = e.dataTransfer;
-
-    for (const item of dt.items) {
-      // If dropped items aren't files, reject them
-      if (item.kind === 'file') {
-        var file = item.getAsFile();
-        let t = await file.text();
-        let j = JSON.parse(t);
-        addGeojson(map, j);
-      }
-    }
-  };
+  document.body.addEventListener('drop', handleDrop(map), false);
 };
 
-start().then(() => {
-  console.log('done');
-});
+start();
